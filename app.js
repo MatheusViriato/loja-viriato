046878aa10004eb30b0acbdb88e7c0b27fa90d27
Matheus Viriato <matheus.nascimento@abril.com.br>
2018-10-21 01:05:28 -0300

    Loja Viriato

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-var express = require('express');
-var consign = require('consign');
-var app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
-app.use(express.static('public'));
-consign().include('app/routes').into(app);
-
-module.exports = app;
+var app = require('./config/server');
 
 app.listen(3000, function(){
     console.log('Servidor on');
